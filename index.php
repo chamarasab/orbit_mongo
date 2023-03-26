0a6e69beca3d9bccedb6e43eccda955570a78e88
chamarasab <chamara.sab@outlook.com>
2023-03-26 11:29:54 +0530

    delete ✔ update ✔

--- index.php
+++ index.php
@@ -10,14 +10,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         if (!empty($_POST["name"]) and !empty($_POST["phone"])) {
             $post_data = array();
-            //$name = $_POST["name"];
-            //$phone = $_POST["phone"];
-            $id = $users->count() + 1;
+            #AutoIncrement ID
+            #db.users.find({},{id:1}).sort( { _id : -1 } ).limit(1);
+            $id = 0;
+
+            $lastuser = $users->find(
+                [
+
+                ],
+                [
+                    '_id' => -1
+                ],
+                [
+                    'id' => 1,
+                    'limit' => 1,
+
+                ]
+            );
+            foreach ($lastuser as $key) {
+                $id = $key["id"];
+            }
+            $id = $id + 1;
             $post_data["id"] = "$id";
             $post_data["name"] = $_POST["name"];
             $post_data["phone"] = $_POST["phone"];
             //$insert_query = "INSERT INTO users (name, phone) VALUES ('" . $name . "','" . $phone . "'); ";
-            
+
             $result = $users->insertOne($post_data); //mysqli_query($connection, $insert_query);
 
             if ($result) {
@@ -47,14 +65,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user_name = "";
     $user_contact = "";
 
-    if(isset($_GET["id"])){
+    if (isset($_GET["id"])) {
         $user_id = $_GET["id"];
         //$select_query = "SELECT * from users WHERE id = '".$user_id."'";
-        $result = $users->find(["id"=>$user_id]);//mysqli_query($connection, $select_query);
+        $result = $users->find(["id" => $user_id]); //mysqli_query($connection, $select_query);
         if ($result) {
             $_SESSION["message"] = "user_found_success";
 
-            foreach($result as $user){
+            foreach ($result as $user) {
                 $user_id = $user['id'];
                 $user_name = $user['name'];
                 $user_contact = $user['phone'];
